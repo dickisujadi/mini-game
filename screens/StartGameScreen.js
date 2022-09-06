e56b02b32a681f7f44c830b6c8f01c0821b1e61d
dickisujadi <dicki.sujadi@eaciit.com>
2022-09-06 11:07:04 +0800

    customButton - styling shadow on android and ios

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,8 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-    return <View>
+    return <View style={styles.inputContainer}>
         <TextInput />
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
@@ -10,3 +10,19 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        padding: 16,
+        marginHorizontal: 24,
+        backgroundColor: '#72063c',
+        borderRadius: 8,
+        elevation: 4, // add shadow on android
+        shadowColor: 'black', // add shadow on ios
+        shadowOffset: { width: 0, height: 2 }, // add shadow on ios
+        shadowRadius: 6, // add shadow on ios
+        shadowOpacity: 0.25, // add shadow on ios
+        // justifyContent: 'center'
+    }
+})
