7baaede0433c62fc3431b5c040647835d3c92e22
dickisujadi <dicki.sujadi@eaciit.com>
2022-09-07 16:46:19 +0800

    textInput - improving buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -9,9 +9,15 @@ function StartGameScreen() {
             keyboardType='number-pad' 
             autoCapitalize="none"
             autoCorrect={false}
-            />
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+        />
+        <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+                <PrimaryButton>Reset</PrimaryButton>
+            </View>
+            <View style={styles.button}>
+                <PrimaryButton>Confirm</PrimaryButton>
+            </View>
+        </View>
     </View>
 }
 
@@ -19,6 +25,8 @@ export default StartGameScreen
 
 const styles = StyleSheet.create({
     inputContainer: {
+        // justifyContent: 'center',
+        alignItems: 'center',
         marginTop: 100,
         padding: 16,
         marginHorizontal: 24,
@@ -41,5 +49,11 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         textAlign: 'center',
         alignContent: 'center'
+    },
+    buttonContainer : {
+        flexDirection: 'row'
+    },
+    button : {
+        // flex: 1
     }
 })
