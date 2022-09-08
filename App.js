6812cb4be66bf566eef3a627db9c9713553a8209
dickisujadi <dicki.sujadi@eaciit.com>
2022-09-08 15:31:47 +0800

    ID-001 linear gradient

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient'
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
+    <LinearGradient colors={['#72063c', '#ddb52f']} style={styles.rootContainer}>
       <StartGameScreen />
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer : {
+    flex: 1,
+  }
+});
